692fb2b38b131ad1c8e41df64ef4c2a9d2e81c79
Aristid Breitkreuz <aristidb@googlemail.com>
2011-12-28 20:41:26 +0100

    add smex

--- init.el
+++ init.el
@@ -42,6 +42,19 @@
 (require 'ido)
 (ido-mode t)
 
+; SMEX (= ido for M-x)
+(add-to-list 'load-path (concat user-emacs-directory "smex"))
+(require 'smex)
+(add-hook 'after-init-hook 'smex-initialize)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c M-x") 'smex-update-and-run)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(smex-initialize) ; only needed when you load this file with M-x eval-buffer
+
 ; TRAMP (some of it from http://tsdh.wordpress.com/2008/08/20/re-open-read-only-files-as-root-automagically/)
 (require 'tramp) ; total requirement :-)
 
