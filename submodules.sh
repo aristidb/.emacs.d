ef4b67d0674e12962d90e00b3075b6b08bce4a07
Aristid Breitkreuz <aristidb@googlemail.com>
2012-01-05 20:42:31 +0100

    also compile everything

--- submodules.sh
+++ submodules.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git submodule init
 git submodule update
-git submodule foreach "git pull origin master ; true"
+git submodule foreach "git pull origin master ; make clean ; make all ; true"
+
+make -C ess-5.14
