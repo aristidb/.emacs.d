7c4f5b8b96a5cea5ad045f652327c1cc30caf933
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-19 18:13:51 +0100

    haskell stuffs

--- init.el
+++ init.el
@@ -270,12 +270,14 @@
 (require 'haskell-mode)
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 (require 'haskell-align-imports)
 (define-key haskell-mode-map (kbd "C-c .") 'haskell-align-imports)
+
+(setq haskell-program-name "/home/aristid/dotfiles/scripts/haskell-repl.pl")
 
 ; Magit
 (require 'magit)
