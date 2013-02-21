e0b995c1abf88be408c0147960ccc8240f79ffff
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-22 00:22:06 +0100

    add list of buffers to subscribe automatically to

--- init.el
+++ init.el
@@ -380,6 +380,15 @@
 (setq weechat-host-default "localhost")
 (setq weechat-port-default 9000)
 
+(setq weechat-auto-monitor-buffers
+      '("euirc.#javacore"
+        "euirc.#c++"
+        "freenode.#haskell"
+        "freenode.#haskell-lens"
+        "freenode.#haskell-blah"
+        "freenode.#startups"
+        "freenode.#nixos"))
+
 (setq weechat-notification-mode t)
 (setq weechat-notification-handler 'weechat-sauron-handler)
 
