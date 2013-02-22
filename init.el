0f7edaf65d32d8c901bf6ea46cf432a828574cb5
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-22 19:18:31 +0100

    forget some initialization for gnuplot in main init.el

--- init.el
+++ init.el
@@ -146,10 +146,6 @@
 (require 'ido)
 (ido-mode t)
 
-;; this line automatically causes all files with the .gp extension to
-;; be loaded into gnuplot mode
-(setq auto-mode-alist (append '(("\\.gp$" . gnuplot-mode)) auto-mode-alist))
-
 ; Backups
 (push (cons "." (concat user-emacs-directory "backups")) backup-directory-alist)
 
--- pkginit/init-gnuplot-mode.el
+++ pkginit/init-gnuplot-mode.el
@@ -2,3 +2,7 @@
 ;; these lines enable the use of gnuplot mode
 (autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
 (autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
+
+;; this line automatically causes all files with the .gp extension to
+;; be loaded into gnuplot mode
+(setq auto-mode-alist (append '(("\\.gp$" . gnuplot-mode)) auto-mode-alist))
