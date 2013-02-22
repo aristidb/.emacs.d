68b5552d327de1800bc2589dbabbd23b0d4bd808
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-22 19:17:12 +0100

    move tramp init into own file

--- init.el
+++ init.el
@@ -146,55 +146,6 @@
 (require 'ido)
 (ido-mode t)
 
-; TRAMP (some of it from http://tsdh.wordpress.com/2008/08/20/re-open-read-only-files-as-root-automagically/)
-(require 'tramp)
-
-(add-to-list 'tramp-remote-path "/run/current-system/sw/bin") ; for NixOS remotes
-
-;; Sudo on remote systems
-(add-to-list 'tramp-default-proxies-alist
-                  '(nil "\\`root\\'" "/ssh:%h:"))
-(add-to-list 'tramp-default-proxies-alist
-                  '("localhost" nil nil))
-
-(defun th-rename-tramp-buffer ()
-  (when (file-remote-p (buffer-file-name))
-    (rename-buffer
-     (format "%s:%s"
-             (file-remote-p (buffer-file-name) 'method)
-             (buffer-name)))))
-
-(add-hook 'find-file-hook
-          'th-rename-tramp-buffer)
-
-(defadvice find-file (around th-find-file activate)
-  "Open FILENAME using tramp's sudo method if it's read-only."
-  (if (and (not (file-writable-p (ad-get-arg 0)))
-           (y-or-n-p (concat "File "
-                             (ad-get-arg 0)
-                             " is read-only.  Open it as root? ")))
-      (th-find-file-sudo (ad-get-arg 0))
-    ad-do-it))
-
-(defun th-find-file-sudo (file)
-  "Opens FILE with root privileges."
-  (interactive "F")
-  (set-buffer (find-file (concat "/sudo::" file))))
-
-; C++ Mode
-(defun aristid-c-mode-common ()
-  (interactive)
-  (font-lock-add-keywords nil
-                          '(("\\<\\(FIXME\\|TODO\\|XXX+\\|BUG\\):"
-                             1 font-lock-warning-face prepend)))
-  (c-set-style "user")
-  (setq c-basic-offset 2)
-  (c-set-offset 'arglist-intro '++)
-  (c-set-offset 'arglist-close 0)
-  (c-set-offset 'label 0))
-
-(add-hook 'c-mode-common-hook 'aristid-c-mode-common)
-
 ;; this line automatically causes all files with the .gp extension to
 ;; be loaded into gnuplot mode
 (setq auto-mode-alist (append '(("\\.gp$" . gnuplot-mode)) auto-mode-alist))
@@ -279,3 +230,7 @@
 ; Auth source
 (require 'auth-source)
 (add-to-list 'auth-sources (concat user-emacs-directory "authinfo.gpg"))
+
+; TRAMP
+(load (concat user-emacs-directory "init/init-tramp.el"))
+
