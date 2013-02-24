a8532389a1376cdeef03e2b3dc2a887decb853bd
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-24 14:31:51 +0100

    ruedi hates long commit messages

--- pkginit/init-magit.el
+++ pkginit/init-magit.el
@@ -1,3 +1,5 @@
 ; Allow git config to demand magit extensions.
 (add-hook 'magit-mode-hook 'magit-load-config-extensions)
+(add-hook 'magit-log-edit-mode-hook 'auto-fill-mode)
+
 (global-set-key (kbd "C-x g") 'magit-status)
