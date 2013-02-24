59f0b6f754f260213051b937207dd42b36c2f305
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-24 11:23:14 +0100

    use visual-line-mode in weechat

--- pkginit/init-weechat.el
+++ pkginit/init-weechat.el
@@ -1,7 +1,8 @@
 (require 'auth-source)
 
-(setq weechat-host-default "localhost")
+(setq weechat-host-default "hermit.breitkreuz.me")
 (setq weechat-port-default 9000)
+(setq weechat-mode-default "ssh -p 22023 -W localhost:%p %h") 
 
 (setq weechat-auto-monitor-buffers
       '("euirc.#javacore"
@@ -17,3 +18,5 @@
 
 (global-set-key (kbd "C-c C-b") 'weechat-switch-buffer)
 ;(set-face-background 'weechat-highlight-face "dark blue")
+
+(add-hook 'weechat-mode-hook 'visual-line-mode)
